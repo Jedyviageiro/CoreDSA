03ab9287f7330df53f48f17593d991002cdb2146
Jedy <jedyviageiro16@gmail.com>
2026-07-13 15:24:49 +0200

    feat: Organize code structure

--- coredsa/src/main/java/com/jedy/coredsa/playground/BankAccount.java
+++ coredsa/src/main/java/com/jedy/coredsa/playground/BankAccount.java
@@ -6,23 +6,24 @@ public class BankAccount {
     private int accountNumber;
     private double balance;
 
-public void addAccountHolder(String name){
-    accountHolder = name;
-}
+    public void addAccountHolder(String name){
+        accountHolder = name;
+    }
 
-public double deposit (double amount){
-    return balance += amount;
-}
+    public double deposit (double amount){
+        return balance += amount;
+    }
 
-public double withdraw (double amount){
-    if(amount > balance){
-        System.out.println("Insufficient funds");
+        public double withdraw (double amount){
+            if(amount > balance){
+                System.out.println("Insufficient funds");
+                return balance;
+            }
+            return balance -= amount;
+        }
+
+    public double getBalance(){
         return balance;
     }
-    return balance -= amount;
-}
 
-public double getBalance(){
-    return balance;
 }
-    }
